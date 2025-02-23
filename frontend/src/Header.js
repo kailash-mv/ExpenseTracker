18b4e9ea7a11154eebc7cfaea18f8f07c9a77241
kailash-mv <158554070+kailash-mv@users.noreply.github.com>
2025-02-23 22:23:51 +0530

    Update Header.js

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,43 +1,21 @@
-// import React from "react";
-// import SearchIcon from "@mui/icons-material/Search";
-// import HeaderOption from "./HeaderOption";
-// import HomeIcon from "@mui/icons-material/Home";
-// import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
-// import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-// import ChatIcon from "@mui/icons-material/Chat";
-// import NotificationsIcon from "@mui/icons-material/Notifications";
-// import { useDispatch } from "react-redux";
-// import { getAuth } from "firebase/auth";
-// import { logout } from "./features/userSlice";
-
-// function Header() {
-//   const dispatch = useDispatch();
-//   const logoutOfApp = () => {
-//     dispatch(logout());
-//     getAuth().signOut();
-//   };
-
-//   return <div className="py-[10px]"></div>;
-// }
-
-// export default Header;
-
 import React, { useState } from "react";
 import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import { AccountCircle } from "@mui/icons-material";
 
 function Header() {
   return (
-    <AppBar position="static" className="text-blue-500">
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          ExpenseTracker+
-        </Typography>
-        <IconButton color="inherit">
-          <AccountCircle />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <div className="min-w-full sm:min-w-auto">
+      <AppBar position="static" className="text-blue-500">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            ExpenseTracker+
+          </Typography>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
